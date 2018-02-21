96a90dd3d48622f06ef1d3a7541b0f7f4d6d7f5a
Statedbump <luis.cintron16@upr.edu>
2018-02-21 13:31:32 -0400

    Area calculation

---
+++ AreaCircle.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class AreaCircle {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double pi = Math.PI;
+		System.out.println("Area of Circle Calculator");
+		System.out.println("Insert the radius of the circle");
+		double radius = sc.nextDouble();
+		System.out.println("the radius is :"+ radius);
+		System.out.println("Calculating....");
+		double area = pi*Math.pow(radius, 2);
+		System.out.println("the area is: "+ area);
+		sc.close();
+		
+		
+		
+	}
+
+}
+
